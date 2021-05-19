67e44f5b177d3356433a50b8d2f0b3e720da2a72
shaunahao <shao@gannett.com>
2021-05-19 15:23:37 -0400

    Add coverage

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # api_test_options
+Mimic local with app.py
+Mimic staging with app_s.py
+
+Run Tavern Test:
+pytest custom_framework/feature_one/test_login.py -v -s
+
+Run Tavern Test:
+pytest snapshot_test/test_login_snap.py -v -s
+
+Run Tavern Test:
+pytest tavern_test/test_login.tavern.yaml -v -s
+
+Run Staging Env:
+pytest {testpath} -v -s --env staging
--- custom_framework/feature_one/test_login.py
+++ custom_framework/feature_one/test_login.py
@@ -1,4 +1,5 @@
 import pytest
+# pip install pytest==4.5.0
 
 def test_success_login(re, url):
     r = re.post(
@@ -9,18 +10,20 @@ def test_success_login(re, url):
     assert r.json() == {"username":"admin", "password":"abc123", "age":18, "sex":"F"}
 
 @pytest.mark.smoke
-def test_login_wrong_sex(re, url):
-    r = re.post(
-        url = url,
-        json = {"username":"admin", "password":"abc123", "age":18, "sex":"X"}
-    )
-    assert r.status_code == 400
-    assert r.json() == {'message': {'sex': 'sex is wrong'}}
+class TestNegativeScenario:
+    def test_login_wrong_sex(self, re, url):
+        r = re.post(
+            url = url,
+            json = {"username":"admin", "password":"abc123", "age":18, "sex":"X"}
+        )
+        assert r.status_code == 400
+        assert r.json() == {'message': {'sex': 'sex is wrong'}}
 
-def test_login_wrong_name(re, url):
-    r = re.post(
-        url = url,
-        json = {"password":"abc123", "age":18, "sex":"F"}
-    )
-    assert r.status_code == 400
-    assert r.json() == {'message': {'username': 'username is wrong'}}
+    @pytest.mark.skip
+    def test_login_wrong_name(self, re, url):
+        r = re.post(
+            url = url,
+            json = {"password":"abc123", "age":18, "sex":"F"}
+        )
+        assert r.status_code == 400
+        assert r.json() == {'message': {'username': 'username is wrong'}}
